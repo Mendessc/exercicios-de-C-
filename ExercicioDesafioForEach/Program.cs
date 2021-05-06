0bf5448154a61559987af8c47794dc18189d8f45
lucas <lucask.silvamendes@gmail.com>
2021-05-06 17:30:26 -0300

    foreach exercicios

---
+++ ExercicioDesafioForEach/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExercicioDesafioForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomeProduto = null;
+            int[] quantidade = null;
+            double[] valor = null;
+
+            Console.WriteLine("Quantos produtos você deseja adicionar?");
+            int quantidadeProdutos = int.Parse(Console.ReadLine());
+
+            nomeProduto = new string[quantidadeProdutos];
+            quantidade = new int [quantidadeProdutos];
+            valor = new double [quantidadeProdutos];
+            
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Digite o nome do {i + 1}ª produto");
+                nomeProduto[i] = (Console.ReadLine());
+            }
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Qual a quantidade do {i + 1}ª produto");
+                quantidade[i] = int.Parse(Console.ReadLine());
+            }
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.WriteLine($"Qual o valor do {i + 1}ª produto");
+                valor[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($@"|====================================================|
+|   Nome              Quantidade             Valor   |  
+|====================================================|");
+
+            for (var i = 0; i < quantidadeProdutos; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($@"|  {nomeProduto[i]}                 {quantidade[i]}                  R${valor[i]} |");
+            }
+            Console.WriteLine($@"|====================================================|");
+        }
+
+    }
+}
---
+++ ExercicioForEach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExercicioForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] embalagens = null;
+
+            Console.WriteLine("Quantas embalagens de pão de queijo tem na prateleira?");
+            int quantidadeEmbalagem = int.Parse(Console.ReadLine());
+
+            embalagens = new int [quantidadeEmbalagem];
+
+            for (var i = 0; i < quantidadeEmbalagem; i++)
+            {
+                Console.WriteLine("Quantos pão de queijo tem na " + (i + 1) + "ª embalagem");
+                embalagens[i] = int.Parse(Console.ReadLine());
+            }
+
+            int c = 1;
+            foreach (var item in embalagens)
+            {
+                Console.WriteLine($"A quantidade de pão de queijo da {c}ª embalagem é: {item}");
+                c++;
+            }
+
+
+        }
+    }
+}
